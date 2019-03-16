cb058770a677b536aa9358a0808a9a4f58ef92e4
Maria Glazova <m_glazova@yahoo.com>
2019-03-17 00:48:53 +0300

    Урок 5. Домашнее задание

---
+++ Lesson5/src/MainClass.java
@@ -0,0 +1,19 @@
+import java.util.concurrent.CyclicBarrier;
+
+public class MainClass {
+    public static final int CARS_COUNT = 4;
+    public static void main(String[] args) {
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
+        Race race = new Race(new Road(60), new Tunnel(), new Road(40));
+        CyclicBarrier cb = new CyclicBarrier(CARS_COUNT);
+        Car[] cars = new Car[CARS_COUNT];
+        for (int i = 0; i < cars.length; i++) {
+            cars[i] = new Car(race, 20 + (int) (Math.random() * 10),cb);
+        }
+        for (int i = 0; i < cars.length; i++) {
+            new Thread(cars[i]).start();
+        }
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
+    }
+}
