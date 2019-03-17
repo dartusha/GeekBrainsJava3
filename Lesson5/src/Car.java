88c639cab468fb489d58110ca8f4b56424d2a22b
Maria Glazova <m_glazova@yahoo.com>
2019-03-17 14:00:43 +0300

    Домашнее задание к уроку 5.

--- Lesson5/src/Car.java
+++ Lesson5/src/Car.java
@@ -1,7 +1,10 @@
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.CyclicBarrier;
 
 public class Car implements Runnable {
     private static int CARS_COUNT;
+    private CountDownLatch cdlStart;
+    private CountDownLatch cdlFinish;
 
     static {
         CARS_COUNT = 0;
@@ -18,12 +21,14 @@ public class Car implements Runnable {
     private CyclicBarrier cb;
 
 
-    public Car(Race race, int speed, CyclicBarrier cb) {
+    public Car(Race race, int speed, CyclicBarrier cb, CountDownLatch cdlStart, CountDownLatch cdlFinish) {
         this.race = race;
         this.speed = speed;
         CARS_COUNT++;
         this.name = "Участник #" + CARS_COUNT;
         this.cb=cb;
+        this.cdlStart=cdlStart;
+        this.cdlFinish=cdlFinish;
     }
     @Override
     public void run() {
@@ -31,6 +36,7 @@ public class Car implements Runnable {
             System.out.println(this.name + " готовится");
             Thread.sleep(500 + (int)(Math.random() * 800));
             System.out.println(this.name + " готов");
+            cdlStart.countDown();
             cb.await();
         } catch (Exception e) {
             e.printStackTrace();
@@ -38,5 +44,14 @@ public class Car implements Runnable {
         for (int i = 0; i < race.getStages().size(); i++) {
             race.getStages().get(i).go(this);
         }
+
+        cdlFinish.countDown();
+
+        if (cdlFinish.getCount()==CARS_COUNT-1){
+            System.out.printf("%s ПОБЕДИЛ!",this.name); //Если ставить сообщение до уменьшения счетчика, в полученный временной промежуток иногда возникает ситуация
+            System.out.println(); //что победителя два. Уменьшаем эту вероятность
+        }
+
+
     }
 }
